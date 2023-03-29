3ccca615fe8f9cf168a4d8b0ac5c805da3e882ce
Arun Sharma <arun.sharmahp89@gmail.com>
2023-03-29 10:32:50 +0530

    modified app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+// import Header from "./Component/DefaultLaout/Header";
+import DefaultLaout from "./Component/DefaultLaout/DefaultLaout";
+import Home from "./Component/home/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Header /> */}
+      <DefaultLaout>
+        <Home />
+      </DefaultLaout>
+    </>
   );
 }
 
